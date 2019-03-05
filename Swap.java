830857443917730f2b16ed4767f562532ae5979d
ChandanaManohar04 <47140337+ChandanaManohar04@users.noreply.github.com>
2019-03-05 14:17:04 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,27 @@
+public class Swap {
+    public static void main(String args[]){
+
+        String str1="happy";
+        String str2="day";
+        String temp="";
+
+        System.out.println("before swapping :");
+        System.out.println("str1 :" +str1);
+        System.out.println("str2 :" +str2);
+
+        temp=str1;
+        str1=str2;
+        str2=temp;
+
+        System.out.println("After Swapping");
+        System.out.println("str1 :" +str1);
+        System.out.println("str2 :" +str2);
+
+
+
+
+    }
+
+
+
+}
